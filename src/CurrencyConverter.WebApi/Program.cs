1468d4ee91f5fbc97349fd23c85025d060e0d9e5
ali zaferany <ali.zaferany79@gmail.com>
2022-09-18 12:46:44 +0430

    init

---
+++ src/CurrencyConverter.WebApi/Program.cs
@@ -0,0 +1,29 @@
+using CurrencyConverter.WebApi;
+using Microsoft.AspNetCore.Builder;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+// NOTE: Default application configuration sources can be found at
+// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-6.0#default-application-configuration-sources
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+// Manually create an instance of the Startup class
+// https://andrewlock.net/exploring-dotnet-6-part-12-upgrading-a-dotnet-5-startup-based-app-to-dotnet-6/
+var startup = new Startup(builder.Configuration);
+
+// Manually call ConfigureServices()
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+// Manually call Configure()
+startup.Configure(app);
+
+app.Run();
+
+
+#pragma warning disable CA1050 // Declare types in namespaces
+// ReSharper disable once ClassNeverInstantiated.Global
+public partial class Program { } //needed for WebApplicationFactory on IntegrationTests
+#pragma warning restore CA1050 // Declare types in namespaces
